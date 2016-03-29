cd0fde0c9d4c101cbfdc82f2a04e1f89365ee024
traustid <traustid@gmail.com>
2016-03-29 15:15:29 +0200

    Font css changes.

--- www/wp-content/themes/riksdagens-motioner/header.php
+++ www/wp-content/themes/riksdagens-motioner/header.php
@@ -15,6 +15,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet" type="text/css">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
