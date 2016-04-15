7c37ef042d926953f8eefe01ad58863cc60c7609
traustid <traustid@gmail.com>
2016-04-15 21:25:14 +0200

    Site css changes.

--- www/wp-content/themes/riksdagens-motioner/page.php
+++ www/wp-content/themes/riksdagens-motioner/page.php
@@ -14,26 +14,18 @@
 
 get_header(); ?>
 
-	<div class="container default-margins page-content">
-		<div class="row">
-			<div class="twelve columns">		
+<?php
+while ( have_posts() ) : the_post();
 
-				<?php
-				while ( have_posts() ) : the_post();
+	get_template_part( 'template-parts/content', 'page' );
 
-					get_template_part( 'template-parts/content', 'page' );
+	// If comments are open or we have at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) :
+		comments_template();
+	endif;
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-
-				endwhile; // End of the loop.
-				?>
-
-			</div>
-		</div>
-	</div>
+endwhile; // End of the loop.
+?>
 
 <?php
 get_sidebar();
--- www/wp-content/themes/riksdagens-motioner/template-parts/content-page.php
+++ www/wp-content/themes/riksdagens-motioner/template-parts/content-page.php
@@ -10,32 +10,53 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php
-			the_content();
+	<div class="entry-header-wrapper">
+		<div class="container default-margins">
+			<div class="row">
+				<div class="twelve columns">
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'riksdagens-motioner' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+					<header class="entry-header">
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					</header><!-- .entry-header -->
+					
+				</div>
+			</div>
+		</div>
+	</div>
 
-	<footer class="entry-footer">
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					esc_html__( 'Edit %s', 'riksdagens-motioner' ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+	<div class="container default-margins page-content">
+		<div class="row">
+			<div class="twelve columns">		
+
+
+				<div class="entry-content">
+					<?php
+						the_content();
+
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'riksdagens-motioner' ),
+							'after'  => '</div>',
+						) );
+					?>
+				</div><!-- .entry-content -->
+
+				<footer class="entry-footer">
+					<?php
+						edit_post_link(
+							sprintf(
+								/* translators: %s: Name of current post */
+								esc_html__( 'Edit %s', 'riksdagens-motioner' ),
+								the_title( '<span class="screen-reader-text">"', '"</span>', false )
+							),
+							'<span class="edit-link">',
+							'</span>'
+						);
+					?>
+				</footer><!-- .entry-footer -->
+
+			</div>
+		</div>
+	</div>
+
+</article>
