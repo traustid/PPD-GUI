26717d0e4bb235c8fa168a2b447e5a7add41555e
traustid <traustid@gmail.com>
2016-05-16 10:42:49 +0200

    Removed label from barchart.

--- www/view-templates/barChartViewTemplate.php
+++ www/view-templates/barChartViewTemplate.php
@@ -1,7 +1,5 @@
 <script id="barChartViewTemplate" type="text/template">
 
-	<p class="text-center text-light"><strong>Documents by parties</strong></p>
-
 	<svg class="chart-container" width="100%" height="200"></svg>
 	
 </script>
